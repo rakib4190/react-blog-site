b696693d88677dbd49a1aae737c9e913bf43ca35
Rakibul Islam <mrislam4141@gmail.com>
2024-07-16 12:53:52 +0600

    show reading time on bookmark component

--- src/components/BookMarks/BookMarks.jsx
+++ src/components/BookMarks/BookMarks.jsx
@@ -3,14 +3,18 @@ import Bookmark from '../Bookmark/Bookmark';
 const BookMarks = ({bookMarks, readingTime}) => {
     
     return (
-        <div className='w-1/3 bg-gray-200 ml-5 pt-5 mt-8'>
-            <div>
-                <h2 className='text-2xl'>Reading Time : {readingTime}</h2>
+        <div className='w-1/3'>
+            <div className='bg-purple-50 ml-5  mt-8 rounded-lg border-2 border-blue-700'>
+                <h2 className='text-2xl text-center font-semibold p-6'>Spent time on read : {readingTime}</h2>
             </div>
+            <div className='bg-gray-200 ml-5 pt-5 mt-8 pb-10'>
             <h2 className='text-2xl font-semibold text-center'>Bookmarked Blogs : {bookMarks.length} </h2>
             {
                 bookMarks.map(bookMark => <Bookmark bookMark={bookMark} key={bookMark.id}></Bookmark>)
             }
+
+            </div>
+
         </div>
     );
 };
