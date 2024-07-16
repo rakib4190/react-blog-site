0251ef6cd1b7ab50c790286c638c9b4a64ab26d9
Rakibul Islam <mrislam4141@gmail.com>
2024-07-16 12:56:29 +0600

    radious added the bookmark component

--- src/components/BookMarks/BookMarks.jsx
+++ src/components/BookMarks/BookMarks.jsx
@@ -7,7 +7,7 @@ const BookMarks = ({bookMarks, readingTime}) => {
             <div className='bg-purple-50 ml-5  mt-8 rounded-lg border-2 border-blue-700'>
                 <h2 className='text-2xl text-center font-semibold p-6'>Spent time on read : {readingTime}</h2>
             </div>
-            <div className='bg-gray-200 ml-5 pt-5 mt-8 pb-10'>
+            <div className='bg-gray-200 ml-5 pt-5 mt-8 pb-10 rounded-lg'>
             <h2 className='text-2xl font-semibold text-center'>Bookmarked Blogs : {bookMarks.length} </h2>
             {
                 bookMarks.map(bookMark => <Bookmark bookMark={bookMark} key={bookMark.id}></Bookmark>)
